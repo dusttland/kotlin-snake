86bdff36e68fe75a2c51c5a8563ba117b930d9a1
Priit Trink <dusttland@gmail.com>
2019-04-14 13:11:54 +0300

    Kotlinx-html somewhat working.

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,10 @@
 import game.Game
 import game.geo.Direction
 import game.snake.*
+import kotlin.browser.document
+import kotlinx.html.*
+import kotlinx.html.dom.create
+import kotlin.browser.window
 
 fun main() {
     println("Hello snake.")
@@ -16,4 +20,26 @@ fun main() {
     println(snake)
 
     Game()
+
+    window.setInterval({
+        val myDiv = document.create.div("panel") {
+            p {
+                +"Here is "
+                a("https://kotlinlang.org") { +"official Kotlin site" }
+            }
+            div {
+                +"Hello writing stuff here."
+            }
+        }
+
+        document.getElementById("container")!!.appendChild(myDiv)
+
+//        not working for some reason
+//        source: https://github.com/Kotlin/kotlinx.html
+//        document.getElementById("container")!!.append {
+//            div {
+//                +"added it"
+//            }
+//        }
+    }, 1000)
 }
